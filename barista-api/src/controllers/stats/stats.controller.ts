b3b023485143972d426298fcb4b191f4dc95de87
Ben Woodring <benajmain.woodring@optum.com>
2020-03-09 13:38:55 -0500

    Add Badges

--- barista-api/src/controllers/stats/stats.controller.ts
+++ barista-api/src/controllers/stats/stats.controller.ts
@@ -1,8 +1,10 @@
 import { Index } from 'typeorm';
-import { Project } from '@app/models';
+import { Project, ProjectScanStatusType, VulnerabilityStatusDeploymentType } from '@app/models';
+import { ProjectScanStatusTypeService } from '@app/services/project-scan-status-type/project-scan-status-type.service';
 import { ProjectService } from '@app/services/project/project.service';
-import { Body, Controller, Get, Param, Post, Query, Request, UseGuards, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Get, Param, Res, Post, Query, Request, UseGuards, UseInterceptors } from '@nestjs/common';
 import { ApiUseTags } from '@nestjs/swagger';
+import { Response } from 'express';
 
 import {
   Crud,
@@ -47,5 +49,83 @@ export class StatsController implements CrudController<Project> {
       }
     }
     return answer;
+  }
+
+  createFormat(label: string, value: string, status: string) {
+    let color = status;
+    if (status === 'unknown') {
+      color = 'lightgrey';
+      value = 'unknown';
+    }
+
+    const format = {
+      text: [label, value],
+      color: color,
+      template: 'flat',
+    };
+
+    return format;
+  }
+
+  createSVG(format: any) {
+    const { BadgeFactory } = require('gh-badges');
+    const bf = new BadgeFactory();
+
+    return bf.create(format);
+  }
+
+  @Get('/badges/:id/licensestate')
+  async getLicenseState(@Param('id') id: string, @Res() res: Response) {
+    const project = await this.service.db.findOne(Number(id));
+
+    let licenseStatus = await ProjectScanStatusTypeService.Unknown();
+    if (project) {
+      licenseStatus = await this.service.highestLicenseStatus(project);
+    }
+
+    const svg = this.createSVG(
+      this.createFormat('barista license state', licenseStatus.createdAt.toDateString(), licenseStatus.code),
+    );
+    return res
+      .status(200)
+      .send(svg)
+      .end();
+  }
+
+  @Get('/badges/:id/securitystate')
+  async getSecurityState(@Param('id') id: string, @Res() res: Response) {
+    const project = await this.service.db.findOne(Number(id));
+
+    let securityStatus = await ProjectScanStatusTypeService.Unknown();
+    if (project) {
+      securityStatus = await this.service.highestSecurityStatus(project);
+    }
+
+    const svg = this.createSVG(
+      this.createFormat('barista security state', securityStatus.createdAt.toDateString(), securityStatus.code),
+    );
+    return res
+      .status(200)
+      .send(svg)
+      .end();
+  }
+
+  @Get('/badges/:id/vulnerabilities')
+  async getvu(@Param('id') id: string, @Res() res: Response) {
+    const project = await this.service.db.findOne(Number(id));
+
+    let securityStatus = await ProjectScanStatusTypeService.Unknown();
+    let valueString = '';
+    if (project) {
+      const vulnerabilities = await this.service.distinctSeverities(project);
+      securityStatus = await this.service.highestSecurityStatus(project);
+      vulnerabilities.forEach(vul => (valueString = valueString + vul.severity + ':' + vul.count + ' '));
+    }
+
+    const svg = this.createSVG(this.createFormat('barista vulnerabilities', valueString, securityStatus.code));
+    return res
+      .status(200)
+      .send(svg)
+      .end();
   }
 }
