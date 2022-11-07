0ec6a2dfd3d50a82a3915ac53cb9c26137dc81ba
Woodring, Benjamin <benjamin.woodring@optum.com>
2022-11-07 12:29:00 -0600

    Update go-licenses.service.ts

--- barista-scan/src/default-scan/scanners/go-licenses/go-licenses.service.ts
+++ barista-scan/src/default-scan/scanners/go-licenses/go-licenses.service.ts
@@ -45,11 +45,15 @@ export class GoLicensesService extends ScannerBaseService {
 
   public convertCsvResultsToJson(csvText: string) {
     let arr = csvText.split('\n');
-    arr = arr.filter(function(item) {
+    arr = arr.filter(function (item) {
       return item.indexOf('E0') !== 0;
     });
 
-    arr = arr.filter(function(item) {
+    arr = arr.filter(function (item) {
+      return item.indexOf('E11') !== 0;
+    });
+
+    arr = arr.filter(function (item) {
       return item.indexOf('-') !== 0;
     });
 
